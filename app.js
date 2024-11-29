c9c044f8bee9861e6c0dafa17730a7aac1f120a7
kingsley <kingsleyjackfestus@gmail.com>
2024-11-29 23:40:00 +0100

    challenge done

--- app.js
+++ app.js
@@ -1,137 +1,339 @@
 // Select every necessary element
-const increaseProductQuantity = document.querySelector('.increase-product-quantity');
-const reduceProductQuantity = document.querySelector('.reduce-product-quantity');
-const gotoPreviousImage = document.querySelector('.go-to-previous-image');
-const closeMobileMenu = document.querySelector('.close-mobile-menu');
-const productQuantity = document.querySelector('.product-quantity');
-const openMobileMenu = document.querySelector('.open-mobile-menu');
-const cartCardHolder = document.querySelector('.cart-card-holder');
-const gotoNextImage = document.querySelector('.go-to-next-image');
-const cartContent = document.querySelector('.cart-content');
-const productImage = document.querySelectorAll('.product');
-const mobileMenu = document.querySelector('.mobile-menu');
-const addToCart = document.querySelector('.add-to-cart');
-const cartBadge = document.querySelector('.cart-badge');
-const cartCard = document.querySelector('.cart-card');
-const cartIcon = document.querySelector('.cart-icon');
-
-const productImageArray = Array.from(productImage)
+const productThumbnailIndex = Array.from(
+  document.querySelectorAll(".thumbnail-cards figure")
+);
+const increaseProductQuantity = document.querySelector(
+  ".increase-product-quantity"
+);
+const reduceProductQuantity = document.querySelector(
+  ".reduce-product-quantity"
+);
+const productImageContainer = document.querySelector(
+  ".product-image-container"
+);
+const productImageLightbox = document.querySelector(".product-image-lightbox");
+const gotoPreviousImage = document.querySelector(".go-to-previous-image");
+const closeMobileMenu = document.querySelector(".close-mobile-menu");
+const productQuantity = document.querySelector(".product-quantity");
+const openMobileMenu = document.querySelector(".open-mobile-menu");
+const cartCardHolder = document.querySelector(".cart-card-holder");
+const gotoNextImage = document.querySelector(".go-to-next-image");
+const cartContent = document.querySelector(".cart-content");
+const mobileMenu = document.querySelector(".mobile-menu");
+const addToCart = document.querySelector(".add-to-cart");
+const cartBadge = document.querySelector(".cart-badge");
+const cartCard = document.querySelector(".cart-card");
+const cartIcon = document.querySelector(".cart-icon");
 
 let productQuantityIndex = 0;
-let currentProductImage = 0;
+let currentProductImage = 1;
 let cartBadgeIndex = 0;
 let totalQuantityInCartIndex = 0;
-
 
 // Hold all resuable functions
 const reusables = {
   // Toggle Mobile Menu
   toggleMobileMenu: (elem, action) => {
-    elem.addEventListener('click', () => {
-      mobileMenu.classList[action]('hidden')
-    })
+    elem.addEventListener("click", () => {
+      mobileMenu.classList[action]("hidden");
+    });
   },
 
   // Toggle Cart
   toggleCartCard: (elem, method, toggle1, toggle2) => {
-    elem.addEventListener('click', (e) => {
+    elem.addEventListener("click", (e) => {
       if (method(e)) {
-        cartCardHolder.classList[toggle1]('hidden')
-        cartCardHolder.classList[toggle2]('grid')
+        cartCardHolder.classList[toggle1]("hidden");
+        cartCardHolder.classList[toggle2]("grid");
       }
-    })
+    });
   },
 
-  // Product Image Scrolling
-  productImageGen: (elem, method1, class1, method2, class2, method3) => {
-    elem.addEventListener('click', () => {
+  // Translate currentProductIndex to background images
+  backgroundProductImages: (img) =>
+    (img.style.cssText += `background: url(images/image-product-${currentProductImage}.jpg) center/cover`),
+
+  // Thumbnail Switch During Scroll
+  thumbnailSwitch: (thumbnails, method) => {
+    thumbnails.filter((value) =>
+      value !== method()
+        ? value.classList.remove("before:absolute", "border-4")
+        : value.classList.add("before:absolute", "border-4")
+    );
+  },
+
+  // Product Image Generator for Desktop View
+  desktopProductImages: function (parent) {
+    parent.lastElementChild.remove();
+    parent.insertAdjacentHTML(
+      "beforeend",
+      `<img class="w-[32rem] rounded-xl" src="images/image-product-${currentProductImage}.jpg" alt="current product image">`
+    );
+    const clonedProductThumbnailIndex = Array.from(
+      parent.nextElementSibling.children
+    );
+    this.thumbnailSwitch(
+      productThumbnailIndex,
+      () => productThumbnailIndex[currentProductImage - 1]
+    );
+    this.thumbnailSwitch(
+      clonedProductThumbnailIndex,
+      () => clonedProductThumbnailIndex[currentProductImage - 1]
+    );
+  },
+
+  // Product Image Scrolling Function
+  productImageGen: function (elem, method1, method2, method3, arg) {
+    elem.addEventListener("click", () => {
       if (method1()) {
-        productImageArray[currentProductImage].classList.add(class1)
-        productImageArray[method2()].classList.remove(class2)
-        method3()
+        method2();
+        this[method3](arg);
       }
-    })
+    });
   },
 
   // Set Product Quantity State
   countProduct: (elem, method1, method2) => {
-    elem.addEventListener('click', () => {
+    elem.addEventListener("click", () => {
       if (method1()) {
         method2();
         productQuantity.value = productQuantityIndex;
       }
-    })
+    });
   },
 
   // Cart Badge State and success notification
   cartBadge: () => {
     cartBadgeIndex = cartBadgeIndex + productQuantityIndex;
     cartBadge.innerHTML = cartBadgeIndex;
-    cartBadge.classList.remove('hidden');
+    cartBadge.classList.remove("hidden");
 
-    document.body.insertAdjacentHTML('beforeend', `
+    document.body.insertAdjacentHTML(
+      "beforeend",
+      `
       <div class="item-added-success-message w-screen fixed top-20 z-20">
-        <p class="w-4/5 m-auto p-4 rounded-lg text-center text-neutral1 text-[clamp(5vw calc(1rem + 2vw))] font-bold bg-green-600">Item successfully added to cart</p>
+        <p class="w-80 m-auto p-4 rounded-lg text-center text-neutral1 font-bold bg-green-600">Item successfully added to cart</p>
       </div>`
     );
     setTimeout(() => {
-      document.querySelector('.item-added-success-message').remove()
+      document.querySelector(".item-added-success-message").remove();
     }, 1000);
   },
 
   // Dynamically create the cards that represent the items added to the cart
   addedItemtoCart: () => {
-    const totalAmount = document.querySelector('.product-price span').innerHTML * productQuantity.value;
+    const totalAmount =
+      document.querySelector(".product-price span").innerHTML *
+      productQuantity.value;
     totalQuantityInCartIndex = productQuantityIndex;
-    cartContent.insertAdjacentHTML('beforeend', `
+    cartContent.insertAdjacentHTML(
+      "beforeend",
+      `
       <div class="cart-item grid grid-cols-[auto_1fr_auto] grid-rows-[2rem_2rem] gap-x-4">
         <img src="images/image-product-1-thumbnail.jpg" alt="cart item" class="w-16 rounded-lg row-span-2">
-        <h4 class="text-secondaryLight self-center text-sm text-nowrap">${document.querySelector('h1').innerHTML}</h4>
+        <h4 class="text-secondaryLight self-center text-sm text-nowrap">${
+          document.querySelector("h1").innerHTML
+        }</h4>
         <img src="images/icon-delete.svg" alt="delete item from cart" class="delete-cart-item row-span-2 self-center">
         <div class="col-start-2 flex items-center">
-        <span class="calculate-price text-secondaryLight self-center">${document.querySelector('.product-price').innerHTML} x ${totalQuantityInCartIndex}</span>
-        <span class="total-price pl-4 text-secondary font-bold">$${totalAmount.toPrecision(String(totalAmount).length + 2)}</span>
+          <span class="calculate-price text-secondaryLight self-center">${
+            document.querySelector(".product-price").innerHTML
+          } x ${totalQuantityInCartIndex}</span>
+          <span class="total-price pl-4 text-secondary font-bold">$${totalAmount.toPrecision(
+            String(totalAmount).length + 2
+          )}</span>
         </div>
       </div>
-      <button class="h-16 rounded-xl text-secondary font-bold bg-primary">Checkout</button>`
-    )
-  }
-}
+      <button class="h-16 rounded-xl text-secondary font-bold bg-primary">Checkout</button>
+      `
+    );
+  },
+};
 
+// Making use of the above 'resusables' object
+
+// Default Displayed Product Image Using 'Translate currentProductIndex to background images'
+document.addEventListener("DOMContentLoaded", () => {
+  reusables.backgroundProductImages(productImageContainer);
+  reusables.thumbnailSwitch(
+    productThumbnailIndex,
+    () => productThumbnailIndex[currentProductImage - 1]
+  );
+});
 
 // Apply Toggle Mobile Menu
-reusables.toggleMobileMenu(openMobileMenu, 'remove');
-reusables.toggleMobileMenu(closeMobileMenu, 'add');
+reusables.toggleMobileMenu(openMobileMenu, "remove");
+reusables.toggleMobileMenu(closeMobileMenu, "add");
 
 // Apply Toggle Cart card
-reusables.toggleCartCard(cartIcon, (e) => e.target, 'remove', 'add')
-reusables.toggleCartCard(cartCardHolder, (e) => e.target === cartCardHolder, 'add', 'remove')
+reusables.toggleCartCard(cartIcon, (e) => e.target, "remove", "add");
+reusables.toggleCartCard(
+  cartCardHolder,
+  (e) => e.target === cartCardHolder,
+  "add",
+  "remove"
+);
 
-// Apply Product Image Scrolling
-reusables.productImageGen(gotoNextImage, () => currentProductImage < productImageArray.length - 1, 'left-full', () => currentProductImage + 1, 'right-full', () => currentProductImage++)
-reusables.productImageGen(gotoPreviousImage, () => currentProductImage > 0, 'right-full', () => currentProductImage - 1, 'left-full', () => currentProductImage--)
+// Apply Translate currentProductIndex to background images
+reusables.productImageGen(
+  gotoNextImage,
+  () => currentProductImage < 4,
+  () => currentProductImage++,
+  "backgroundProductImages",
+  productImageContainer
+);
+reusables.productImageGen(
+  gotoPreviousImage,
+  () => currentProductImage > 1,
+  () => currentProductImage--,
+  "backgroundProductImages",
+  productImageContainer
+);
 
 // Apply Product Quantity State
-reusables.countProduct(increaseProductQuantity, () => productQuantityIndex >= 0, () => productQuantityIndex++);
-reusables.countProduct(reduceProductQuantity, () => productQuantityIndex >= 1, () => productQuantityIndex--);
+reusables.countProduct(
+  increaseProductQuantity,
+  () => productQuantityIndex >= 0,
+  () => productQuantityIndex++
+);
+reusables.countProduct(
+  reduceProductQuantity,
+  () => productQuantityIndex >= 1,
+  () => productQuantityIndex--
+);
 
 // Add to cart button on-click
-addToCart.addEventListener('submit', (e) => {
+addToCart.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  if (productQuantity.value >= 1 && !cartContent.classList.contains('not-empty')) {
-    document.querySelector('.empty-cart-message').classList.add('hidden')
-    cartContent.classList.add('not-empty')
-    reusables.addedItemtoCart()
-    reusables.cartBadge()
-  }
-  else if (productQuantity.value >= 1 && cartContent.classList.contains('not-empty')) {
-    reusables.cartBadge()
-    totalQuantityInCartIndex = Number(totalQuantityInCartIndex) + Number(productQuantity.value);
-    const totalAmount = document.querySelector('.product-price span').innerHTML * totalQuantityInCartIndex;
-    document.querySelector('.calculate-price').innerHTML = `${document.querySelector('.product-price').innerHTML} x ${totalQuantityInCartIndex}`
-    document.querySelector('.total-price').innerHTML = `$${totalAmount.toPrecision(String(totalAmount).length + 2)}`;
+  if (
+    productQuantity.value >= 1 &&
+    !cartContent.classList.contains("not-empty")
+  ) {
+    document.querySelector(".empty-cart-message").classList.add("hidden");
+    cartContent.classList.add("not-empty");
+    reusables.addedItemtoCart();
+    reusables.cartBadge();
+  } else if (
+    productQuantity.value >= 1 &&
+    cartContent.classList.contains("not-empty")
+  ) {
+    reusables.cartBadge();
+    totalQuantityInCartIndex =
+      Number(totalQuantityInCartIndex) + Number(productQuantity.value);
+    const totalAmount =
+      document.querySelector(".product-price span").innerHTML *
+      totalQuantityInCartIndex;
+    document.querySelector(".calculate-price").innerHTML = `${
+      document.querySelector(".product-price").innerHTML
+    } x ${totalQuantityInCartIndex}`;
+    document.querySelector(
+      ".total-price"
+    ).innerHTML = `$${totalAmount.toPrecision(String(totalAmount).length + 2)}`;
   }
   productQuantityIndex = 0;
   productQuantity.value = 0;
-})
+});
+
+// thumbnail and cloned elements
+productThumbnailIndex.forEach((thumbnail) => {
+  thumbnail.addEventListener("click", (e) => {
+    reusables.thumbnailSwitch(productThumbnailIndex, () => e.target);
+
+    const clonedLightbox = productImageLightbox.cloneNode(true);
+    const clonedProductImageContainer = clonedLightbox.children[0];
+    const clonedProductThumbnailContainer = clonedLightbox.children[1];
+    const closeFullScreenImage = clonedProductImageContainer.children[0];
+    const clonedGotoPreviousImage = clonedProductImageContainer.children[1];
+    const clonedGotoNextImage = clonedProductImageContainer.children[2];
+    const clonedProductThumbnailIndex = Array.from(
+      clonedProductThumbnailContainer.children
+    );
+
+    currentProductImage = productThumbnailIndex.indexOf(thumbnail) + 1;
+
+    clonedLightbox.classList.add(
+      "cloned-lightbox",
+      "w-screen",
+      "fixed",
+      "top-0",
+      "left-0",
+      "z-20",
+      "bg-neutral2Light",
+      "place-content-center",
+      "lg:h-screen"
+    );
+
+    clonedLightbox.style.cssText += `
+    grid-template-columns: 1fr auto 1fr;
+    grid-template-rows: auto 5.5rem;
+    place-content:center;
+    place-items: center;
+    row-gap: 2rem`;
+
+    clonedProductImageContainer.style.cssText += `
+    background-image: inherit;
+    grid-area: 1/2/2/3;`;
+
+    clonedProductThumbnailContainer.style.cssText = `
+    grid-column-start: 2;
+    justify-self: stretch;
+    padding-inline: 6.5rem`;
+
+    clonedGotoNextImage.style.cssText = `
+    order: 1;
+    display: grid;
+    box-sizing: content-box;
+    transform: scale(1) translate(-1.8rem)`;
+
+    clonedGotoPreviousImage.style.cssText = `
+    display: grid;
+    box-sizing: content-box;
+    transform: scale(1) translate(1.8rem)`;
+
+    closeFullScreenImage.style.cssText += `display: block`;
+
+    clonedProductImageContainer.insertAdjacentHTML(
+      "beforeend",
+      `<img class="w-[32rem] rounded-xl" src="images/image-product-${currentProductImage}.jpg" alt="current product image">`
+    );
+
+    closeFullScreenImage.addEventListener("click", () => {
+      clonedLightbox.style.cssText = `display: none`;
+      productImageContainer.style.cssText += `background-image: url(images/image-product-${currentProductImage}.jpg)`;
+    });
+
+    reusables.productImageGen(
+      clonedGotoNextImage,
+      () => currentProductImage < 4,
+      () => currentProductImage++,
+      "desktopProductImages",
+      clonedProductImageContainer
+    );
+    reusables.productImageGen(
+      clonedGotoPreviousImage,
+      () => currentProductImage > 1,
+      () => currentProductImage--,
+      "desktopProductImages",
+      clonedProductImageContainer
+    );
+
+    document.body.append(clonedLightbox);
+
+    clonedProductThumbnailIndex.forEach((value) => {
+      value.addEventListener("click", (e) => {
+        reusables.thumbnailSwitch(clonedProductThumbnailIndex, () => e.target);
+        currentProductImage = clonedProductThumbnailIndex.indexOf(value) + 1;
+        clonedProductImageContainer.lastElementChild.remove();
+        clonedProductImageContainer.insertAdjacentHTML(
+          "beforeend",
+          `<img class="w-[32rem] rounded-xl" src="images/image-product-${currentProductImage}.jpg" alt="current product image">`
+        );
+        reusables.thumbnailSwitch(
+          productThumbnailIndex,
+          () => productThumbnailIndex[currentProductImage - 1]
+        );
+      });
+    });
+  });
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,25 +4,36 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        main: ['Kumbh Sans', 'sans-serif']
+        main: ["Kumbh Sans", "sans-serif"],
       },
+
       colors: {
-        primary: 'hsl(26, 100%, 55%)',
-        primaryLight: 'hsl(25, 99%, 73%)',
-        secondary: 'hsl(220, 13%, 13%)',
-        secondaryLight: 'hsl(219, 9%, 45%)',
-        backup: 'hsl(223, 64%, 98%)',
-        neutral1: 'hsl(0, 0%, 100%)',
-        neutral2: 'hsl(0, 0%, 0%)',
-        neutral2Light: '#000000b3',
+        primary: "hsl(26, 100%, 55%)",
+        primaryLight: "hsl(25, 99%, 73%)",
+        secondary: "hsl(220, 13%, 13%)",
+        secondaryLight: "hsl(219, 9%, 45%)",
+        backup: "hsl(223, 64%, 98%)",
+        neutral1: "hsl(0, 0%, 100%)",
+        neutral2: "hsl(0, 0%, 0%)",
+        neutral2Light: "#000000b3",
       },
+
       boxShadow: {
-        'custom-shadow': '0 2rem 2rem hsla(26, 100%, 55%, 0.302)',
-      }
+        "custom-shadow": "0 2rem 2rem hsla(26, 100%, 55%, 0.302)",
+      },
+
+      backgroundImage: {
+        "default-image": "url('images/image-product-1.jpg')",
+      },
+
+      filter: {
+        "white-filter": "invert(100%) brightness(200%)",
+        "primary-filter":
+          "invert(60%) sepia(90%) saturate(500%) hue-rotate(0deg) brightness(95%) contrast(100%)",
+      },
     },
   },
-  plugins: [],
-}
-
+  plugins: [require("tailwindcss-filters")],
+};
 
 // - Black (with 75% opacity for lightbox background): hsl(0, 0%, 0%)
